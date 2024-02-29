73dd835787ab25e4f79ec38adea515feb4cd416e
Domenico <domenicoribaudo06@gmail.com>
2024-02-29 16:10:33 +0100

    commit finale, esercizio completato

--- src/main/java/com/develhope/demoesercizioGetCompleta/ControllerSalutaUser.java
+++ src/main/java/com/develhope/demoesercizioGetCompleta/ControllerSalutaUser.java
@@ -1,5 +1,15 @@
 package com.develhope.demoesercizioGetCompleta;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/v2")
 public class ControllerSalutaUser {
+
+   @GetMapping("/ciao/{provincia}")
+    public User saluta(@PathVariable String provincia, @RequestParam String nome ){
+       String stringaSaluto = "Ciao " + nome + " com'è il tempo in " + provincia + "?";
+       User user = new User(nome, provincia, stringaSaluto);
+       return user;
+   }
 
 }
